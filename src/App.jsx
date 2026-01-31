325b497da87a59c22a247324ff5e8151d8d8580b
Maitri dave <maitriddave2511@gmail.com>
2026-01-31 13:00:52 +0530

    [MD] [files are added]

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import Register from './pages/Register'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <Register/>
-    <Header/>
-    </>
-  )
+  const route = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+  return <RouterProvider router={route} />;
 }
 
-export default App
+export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,65 @@
 import React from "react";
+import "./Register.css";
 
 const Register = () => {
   return (
-  <>
-    <div className="form-container">
-    <h1>this is Register</h1>
-    </div>
-  </>
+    <>
+      <div className="form-container">
+        {/* Page Title*/}
+        <h1 className="form-title">Register</h1>
+        <form action="">
+          <div className="form-group">
+            <label htmlFor="">Full Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter your full name "
+            />
+          </div>
+
+          {/*Email*/}
+          <div className="form-group">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your Email Address"
+            />
+          </div>
+
+          {/*Mobile*/}
+          <div className="form-group">
+            <label htmlFor="phone numberx">Mobile Number</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="Enter your Mobile Number"
+            />
+          </div>
+
+          {/*password*/}
+          <div className="form-group">
+            <label htmlFor="password">Mobile Number</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Create your password"
+            />
+          </div>
+
+          {/*submit button*/}
+          <button type="submit" className="btn-primary">
+            Register
+          </button>
+        </form>
+
+        {/*link to log in page*/}
+      </div>
+    </>
   );
 };
 export default Register;
