5a63a10c3c9f3438b21a6818d68a71c4dab0b30f
Maitri dave <maitriddave2511@gmail.com>
2026-02-02 13:31:22 +0530

    [MD] [one file are added]

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,22 @@ import "./App.css";
 import Header from "./components/Header";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+
+const DefaultRoute = () => {
+  const authData = JSON.parse(localStorage.getItem('authData'));
+  if(authData){
+    return <Navigate to ="/login" replace/>
+  }
+  return <Navigate to = "/register" replace/>
+}
 
 function App() {
   const route = createBrowserRouter([
+    {
+      path :"/",
+      element :<DefaultRoute/>
+    },
     {
       path: "/login",
       element: <Login />,
