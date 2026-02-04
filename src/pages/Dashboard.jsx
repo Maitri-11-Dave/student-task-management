6a72e1b9810b835b66dbd5f44234a85e0ecbb30b
Maitri dave <maitriddave2511@gmail.com>
2026-02-04 13:42:47 +0530

    [MD] [two files are added]

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import TaskList from "../components/TastList";
+import { resumeToPipeableStream } from "react-dom/server";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = responce.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    localStorage.removeItem("authData");
+    // localStorage.clear()
+    navigate("/login");
+  };
+
   return (
-    <div><h1><b>Hello Users</b></h1></div>
-  )
-}
+    <div>
+      <Navbar title="Task Manager" onLogout={handleLogout} />
+      <h1>MY TASK</h1>
+      <TaskList />
+    </div>
+  );
+};
+
+export default Dashboard;
